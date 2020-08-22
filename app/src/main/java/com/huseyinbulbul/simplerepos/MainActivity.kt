5a38cc6956d339ff219328b272e9067aa239218a
1hsyn1 <hsyn1991@gmail.com>
2020-08-22 15:10:00 +0300

    initial commit with libraries

---
+++ app/src/main/java/com/huseyinbulbul/simplerepos/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.huseyinbulbul.simplerepos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
